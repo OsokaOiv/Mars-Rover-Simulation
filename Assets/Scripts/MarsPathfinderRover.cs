6d58c70e07424000163e686af856faff7f7b4345
OsokaOiv <66778967+OsokaOiv@users.noreply.github.com>
2021-11-07 22:17:32 +0100

    Finale Version für die W-Seminar Arbeit
    Kleine Anpassungen in den Kommentaren

--- Assets/Scripts/MarsPathfinderRover.cs
+++ Assets/Scripts/MarsPathfinderRover.cs
@@ -1,30 +1,30 @@
 using System.Collections;
-using UnityEngine;
+using UnityEngine; // Bibliothek mit vorgefertigten Methoden
 
 public class MarsPathfinderRover : MonoBehaviour {
     #region Attribute
-    public int TimeMultiplier = 100; // Geschwindigkeit der Zeit
+    public int TimeMultiplier = 100; // Zeitmultiplikator
     [SerializeField] private LayerMask GroundLayer;
     [SerializeField] private string GroundName;
     private const float MovementSpeed = .0067f; // 0,67 cm/s
     private const float SegmentLength = .065f; // 6,5 cm
-    private const float RoverDiskRadius = 1f; // 2,5 m
+    private const float RoverDiskRadius = 1f; // 1 m
     private const float ScanningTime = 120; // 120 s
     private const float RoverHeightAboveGround = .05f;
     private const float MaxTurnAngle = 1;
     private const float RotationSpeed = .2f;
     private float DistanceToDrive = 0;
-    private int Hazards = 0; // Anzahl Hindernisse
+    private int Hazards = 0; // Anzahl Hindernisse im Sichtfeld
     private bool ShouldTurnToWaypoint = true; // Wird temporär auf false gesetzt, wenn ein Hindernis im Weg ist
     #endregion
     // Wird zur Initialisierung aufgerufen
-    void Start() {
+    private void Start() {
         ParallelToGround(); // Rover parallel zum Boden orientieren
         StartCoroutine(ScanningCoroutine());
     }
 
     // Die Kontrollschleife wird wiederholt aufgerufen
-    void Update() {
+    private void Update() {
         if (Waypoint.Instance.Waypoints.Count > 0) { // Wenn es mehr als 0 Wegpunkte gibt
             if (DistanceToDrive > 0 && Hazards == 0) { // Wenn er fahren kann
                 if (WaypointReached(Waypoint.Instance.Waypoints.Peek())) { // Wenn der Wegpunkt erreicht wurde
@@ -44,7 +44,7 @@ public class MarsPathfinderRover : MonoBehaviour {
         }
     }
 
-    // Simuliert Scanzeit und beendet Scan, wenn es keine Hindernisse mehr gibt
+    // Simuliert Scanzeit & beendet Scan bei keinen Hindernissen
     IEnumerator ScanningCoroutine() {
         yield return new WaitForSeconds(ScanningTime / TimeMultiplier);
         if (Hazards == 0) { NoHazardInView(); }
@@ -68,10 +68,10 @@ public class MarsPathfinderRover : MonoBehaviour {
 
     // Rotiert den Rover zum Wegpunkt
     private void RotateTowardsWaypoint(Vector3 waypoint) {
-        // Berechnung vom Vector von der Rover Position zum Wegpunkt
+        // Berechnung vom VeKtor von der Rover Position zum Wegpunkt
         Vector3 vectorToWaypoint = waypoint - transform.position;
 
-        // Konvertieren von 3D zu 2D Koordinate der Vectoren
+        // Konvertieren von 3D zu 2D Koordinate der Vektoren
         Vector2 vectorToWaypoint2D = new Vector2(vectorToWaypoint.x, vectorToWaypoint.z);
         Vector2 forward2D = new Vector2 (transform.forward.x, transform.forward.z);
 
@@ -79,10 +79,10 @@ public class MarsPathfinderRover : MonoBehaviour {
         float radians = Mathf.Acos(Vector2.Dot(vectorToWaypoint2D.normalized, forward2D.normalized));
         float angle = radians * Mathf.Rad2Deg;
 
-        // Maximale Drehung von maxTurnAngle
+        // Maximale Drehung von MaxTurnAngle
         angle = angle < MaxTurnAngle ? angle : MaxTurnAngle;
 
-        // Beurteilung ob diese Drehung nach rechts oder links erfolgen soll
+        // Beurteilung ob Drehung nach rechts oder links erfolgen soll
         Vector2 right2D = new Vector2(transform.right.x, transform.right.z);
         Vector2 left2D = -right2D;
         if (Mathf.Acos(Vector2.Dot(vectorToWaypoint2D.normalized, left2D.normalized))*Mathf.Rad2Deg < 
@@ -97,10 +97,10 @@ public class MarsPathfinderRover : MonoBehaviour {
     // Gibt true zurück wenn der Wegpunkt erreicht wurde
     private bool WaypointReached(Vector3 waypoint)
     {
-        // Wenn die x-Koordinate vom Wegpunkt nicht in der 5m Scheibe ist
+        // Wenn die x-Koordinate vom Wegpunkt nicht in der Scheibe ist
         if (waypoint.x <= transform.position.x - RoverDiskRadius || waypoint.x >= transform.position.x + RoverDiskRadius)
         { return false; }
-        // Wenn die z-Koordinate vom Wegpunkt nicht in der 5m Scheibe ist
+        // Wenn die z-Koordinate vom Wegpunkt nicht in der Scheibe ist
         else if (waypoint.z <= transform.position.z - RoverDiskRadius || waypoint.z >= transform.position.z + RoverDiskRadius) 
         { return false; }
         else { return true; }
@@ -125,7 +125,7 @@ public class MarsPathfinderRover : MonoBehaviour {
 
     // Stellt die Distanz ein
     private void NoHazardInView() {
-        DistanceToDrive = SegmentLength * 5;
+        DistanceToDrive = SegmentLength;
         ShouldTurnToWaypoint = true;
     }
 
